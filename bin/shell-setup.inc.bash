f87570f24eba316af428239a5285043babbbc8d6
Benjamin Gandon <benjamin@gandon.org>
2017-10-10 16:13:46 +0200

    Add more missing stuff

--- bin/shell-setup.inc.bash
+++
@@ -1,5 +0,0 @@
-bosh2 -e "$(bosh2 int "$BASE_DIR/conf/env-depl-vars.yml" --path /internal_ip)" \
-    --ca-cert <(bosh2 int "$BASE_DIR/state/env-creds.yml" --path /director_ssl/ca) \
-    alias-env "$BOSH_ENVIRONMENT_ALIAS"
-export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=$(bosh2 int "$BASE_DIR/state/env-creds.yml" --path /admin_password)
