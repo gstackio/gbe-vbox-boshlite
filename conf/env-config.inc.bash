1c3b8f705f34be1d302cdcbd39a2e130fd07b778
Benjamin Gandon <benjamin@gandon.org>
2017-10-10 15:55:22 +0200

    Add missing parts

--- conf/env-config.inc.bash
+++ conf/env-config.inc.bash
@@ -1,4 +1,4 @@
 # Choose here your upstream deployment directory
-export UPSTREAM_DEPLOYMENT_DIR=$PWD/../../bosh-deployment
+export UPSTREAM_DEPLOYMENT_DIR=$PWD/../bosh-deployment
 export MAIN_DEPLOYMENT_FILE=$UPSTREAM_DEPLOYMENT_DIR/bosh.yml
 export BOSH_ENVIRONMENT_ALIAS=vbox
--- lib/common.inc.bash
+++ lib/common.inc.bash
@@ -1,40 +1,29 @@
-# We assume we have those variables set:
-#  - BASE_DIR
-#  - UPSTREAM_DEPLOYMENT_DIR
-#  - MAIN_DEPLOYMENT_FILE
-# These are typicaly set by the '.envrc' config file
 
-source "$BASE_DIR/conf/env-operations-layout.inc.bash"
+function assert() {
+    local dir_type=$1 # 'deployment', 'cloud-config' or 'runtime-config'
+    local asserted_value=$2 # value not to be empty
+
+    if [ -z "$asserted_value" ]; then
+        echo "$(basename $0): ERROR: not in a $dir_type directory. Aborting." >&2
+        exit 1
+    fi
+}
+
+function restrict_permissions() {
+    local filename=$1
+
+    >> "$filename"
+    chmod 600 "$filename"
+}
 
 function build_operations_arguments() {
     operations_arguments=()
-    for op in "${local_operations[@]}"; do
-        operations_arguments+=(-o "$BASE_DIR/operations/$op.yml")
-    done
 
     for op in "${upstream_operations[@]}"; do
         operations_arguments+=(-o "$UPSTREAM_DEPLOYMENT_DIR/$op.yml")
     done
-}
 
-function bosh2_ro_invoke() {
-    local verb=$1
-    shift
-
-    build_operations_arguments
-
-    bosh2 "$verb" "$UPSTREAM_DEPLOYMENT_DIR/bosh.yml" \
-        "${operations_arguments[@]}" \
-        -l "$BASE_DIR/conf/env-depl-vars.yml" \
-        "$@"
-}
-
-function bosh2_rw_invoke() {
-    local verb=$1
-    shift
-
-    bosh2_ro_invoke "$verb" \
-        --vars-store "$BASE_DIR/state/env-creds.yml" \
-        --state "$BASE_DIR/state/env-infra-state.json" \
-        "$@"
+    for op in "${local_operations[@]}"; do
+        operations_arguments+=(-o "$DEPL_DIR/operations/$op.yml")
+    done
 }
